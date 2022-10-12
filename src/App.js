cb6150d4e4bda3809f0f54b8be1dbecfcb80b991
Farida-Hamid <faridaaddala@gmail.com>
2022-10-12 19:13:34 +0300

    Setup Linters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-class App extends Component {
+class App extends PureComponent {
   render() {
     return (
       <h1>Hello World</h1>
-      );
-    }
+    );
   }
-  
+}
+
 export default App;
