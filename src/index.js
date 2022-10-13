f80f05ffba9e851301fe04d2cc04a9688ce6f1d0
Farida-Hamid <faridaaddala@gmail.com>
2022-10-13 09:44:35 +0300

    Prep index.js for store

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import React from 'react'; import ReactDOM from 'react-dom'; import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Provider>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+);
