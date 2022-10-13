6198ae2add3dc2b57718eaab07dc0649cd4d5651
Farida-Hamid <faridaaddala@gmail.com>
2022-10-13 10:44:46 +0300

    Adjust App and index to read from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React, { PureComponent } from 'react';
 
-class App extends PureComponent {
-  render() {
-    return (
+import { recieveLeagues } from './Redux/teams';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+function App() {
+  const leagueList = useSelector(state => state);
+  const dispatch = useDispatch();
+  console.log(leagueList);
+
+  useEffect(() => async () => {
+    await dispatch(recieveLeagues());
+  }, []);
+
+  return (
+    <div>
       <h1>Hello World</h1>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
+import store from './Redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <Provider>
+      <Provider store={store}>
         <App />
       </Provider>
     </Router>
